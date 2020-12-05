44cef04753f0af4f2fd807e44d0e6fa83fbdf0f7
Alexandre Girard <alexandre.girard@etudiant.univ-lr.fr>
2020-12-05 14:58:01 +0100

    Adding flask_restplus

--- api/app.py
+++ api/app.py
@@ -1,8 +1,9 @@
 from flask import request, Flask
+from flask_restplus import Api, Resource, fields
 import json
 
 app = Flask(__name__)
-
+api = Api(app, version="1.0", title='CleanCode API', description='Evaluated API')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
